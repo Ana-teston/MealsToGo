beab80d1917ab2a40824fd98339e87a0051b8666
Ana-teston <anapaulateston@gmail.com>
2022-06-22 21:05:20 +0200

    Buttons navigation Login and Register

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-import {AccountBackground} from "../components/account.styles";
+import {AccountBackground, AccountContainer, AccountCover, AuthButton} from "../components/account.styles";
+import {Spacer} from "../../../components/spacer/spacer.component";
 
-export const AccountScreen = () => {
-    return <AccountBackground />;
+export const AccountScreen = ({navigation}) => {
+    return (
+        <AccountBackground>
+            <AccountCover />
+            <AccountContainer>
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={() => navigation.navigate("Login")}
+                >
+                    Login
+                </AuthButton>
+                <Spacer size="large">
+                    <AuthButton
+                        icon="lock-open-outline"
+                        mode="contained"
+                        onPress={() => navigation.navigate("Register")}
+                    >
+                        Register
+                    </AuthButton>
+                </Spacer>
+            </AccountContainer>
+        </AccountBackground>
+    );
 };
